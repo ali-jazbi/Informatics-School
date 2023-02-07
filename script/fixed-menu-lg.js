08353f9442cb64447b3f68004437df2a65e47d52
Ali Jazbi <ali.jazbi@yahoo.com>
2023-02-08 00:17:10 +0330

    change navbar pc

--- script/fixed-menu-lg.js
+++ script/fixed-menu-lg.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
             navbar_height = document.querySelector('.navbar').offsetHeight;
             document.body.style.paddingTop = navbar_height + 'px';
         } else {
-             document.getElementById('navbar_top').classList.remove('fixed-top');
+            document.getElementById('navbar_top').classList.remove('fixed-top');
              // remove padding top from body
             document.body.style.paddingTop = '0';
         } 
