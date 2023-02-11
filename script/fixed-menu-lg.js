bb1e00f9508c457c883fdcea7d2795ac008bd2f2
Ali Jazbi <ali.jazbi@yahoo.com>
2023-02-11 16:46:39 +0330

    basic mobile navbar completed

--- script/fixed-menu-lg.js
+++ script/fixed-menu-lg.js
@@ -6,8 +6,12 @@ window.addEventListener("scroll", function() {
     if (scrollPos > 100) {
       document.querySelector(".pc-navbar").style.backgroundColor = "rgba(255, 255, 255, 0.929)";
       document.querySelector(".pc-navbar").style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.1)";
+      document.querySelector(".mobile-navbar").style.backgroundColor = "rgba(255, 255, 255, 0.929)";
+      document.querySelector(".mobile-navbar").style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.1)";
     } else {
       document.querySelector(".pc-navbar").style.backgroundColor = "white";
       document.querySelector(".pc-navbar").style.boxShadow = "none";
+      document.querySelector(".mobile-navbar").style.backgroundColor = "white";
+      document.querySelector(".mobile-navbar").style.boxShadow = "none";
     }
   });  
