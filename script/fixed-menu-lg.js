417f4d96917d293c47f59d9e441bc9f80afa69e4
Ali Jazbi <ali.jazbi@yahoo.com>
2023-02-11 14:53:11 +0330

    fullpage dropdown hover completed

--- script/fixed-menu-lg.js
+++ script/fixed-menu-lg.js
@@ -1,23 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-		
-    window.addEventListener('scroll', function() {
-       
-        if (window.scrollY > 300) {
-            document.getElementById('navbar_top').classList.add('fixed-top');
-            // add padding top to show content behind navbar
-            navbar_height = document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-            document.getElementById('navbar_top').classList.remove('fixed-top');
-             // remove padding top from body
-            document.body.style.paddingTop = '0';
-        } 
-    });
-}); 
-$(document).ready(function(){
-    $('.dropdown-submenu a.test').on("click", function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-    });
-   });
+window.addEventListener("scroll", function() {
+    // Get the current scroll position
+    var scrollPos = window.scrollY;
+    
+    // Perform actions based on the scroll position
+    if (scrollPos > 100) {
+      document.querySelector(".pc-navbar").style.backgroundColor = "rgba(255, 255, 255, 0.929)";
+      document.querySelector(".pc-navbar").style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.1)";
+    } else {
+      document.querySelector(".pc-navbar").style.backgroundColor = "white";
+      document.querySelector(".pc-navbar").style.boxShadow = "none";
+    }
+  });  
