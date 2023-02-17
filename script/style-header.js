77b19c9475c13308fac87300f1c27d12978c64a4
Ali Jazbi <ali.jazbi@yahoo.com>
2023-02-17 18:20:40 +0330

    add banner button

---
+++ script/style-header.js
@@ -0,0 +1,39 @@
+new FinisherHeader({
+    "count": 90,
+    "size": {
+      "min": 1,
+      "max": 32,
+      "pulse": 0
+    },
+    "speed": {
+      "x": {
+        "min": 0,
+        "max": 0.3
+      },
+      "y": {
+        "min": 0,
+        "max": 0.1
+      }
+    },
+    "colors": {
+      "background": "#008545",
+      "particles": [
+        "#ffffff",
+        "#87ddfe",
+        "#acaaff",
+        "#1bffc2",
+        "#f88aff"
+      ]
+    },
+    "blending": "screen",
+    "opacity": {
+      "center": 0,
+      "edge": 0.25
+    },
+    "skew": 0,
+    "shapes": [
+      "c",
+      "s",
+      "t"
+    ]
+  });
