2f02c2bf6d445fbc2f474e1ff83a3da1de3b8e4a
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-16 18:27:25 +0330

    add new fade scroll effect

--- script/loader.js
+++ script/loader.js
@@ -1,3 +1,5 @@
 window.addEventListener('load', function(){
         document.querySelector('.loader').classList.add('hide');
+        document.body.style.overflow = 'auto';
 });
+document.body.style.overflow = 'hidden';
