e37dd0fb6c8a9f3667b67fd4e7154401937206ce
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-16 16:10:33 +0330

    update loader time 3s

--- script/loader.js
+++ script/loader.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', function(){
     document.querySelector('.loader').classList.add('hide');
 });
+var myVar;
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+function showPage() {
+    document.querySelector('.loader').style.display = "none";
+}
