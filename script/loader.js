e977e2b139aa94717d6e7f482d3165eceb7945a9
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-17 17:09:56 +0330

    add simple and preview footer

--- script/loader.js
+++ script/loader.js
@@ -3,5 +3,4 @@ window.addEventListener('load', function(){
         document.querySelector('.loader').classList.add('hide');
         document.querySelector('main').style.display = 'block'; 
         document.querySelector('body').style.overflowY = 'scroll';
-        document.querySelector('body').style.height = '3000px';
 });
