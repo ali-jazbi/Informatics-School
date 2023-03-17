f9d212f4e6f1ef98940e4769d06d9ca8cc115f35
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-17 16:16:04 +0330

    Update loader.js

--- script/loader.js
+++ script/loader.js
@@ -1,6 +1,8 @@
+document.querySelector('body').style.overflow = 'hidden';
 document.querySelector('main').style.display = 'none';
 document.querySelector('body').style.height = 'auto';
 window.addEventListener('load', function(){
         document.querySelector('.loader').classList.add('hide');
         document.querySelector('main').style.display = 'block'; 
+        document.querySelector('body').style.overflow = 'auto';
 });
