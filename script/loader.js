becf02fed950d0a8b1e1d13171e992d9d9550efb
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-19 02:15:42 +0330

    test scroll bug

--- script/loader.js
+++ script/loader.js
@@ -2,5 +2,4 @@ document.querySelector('main').style.display = 'none';
 window.addEventListener('load', function(){
         document.querySelector('.loader').classList.add('hide');
         document.querySelector('main').style.display = 'block'; 
-        document.querySelector('body').style.overflowY = 'scroll';
 });
