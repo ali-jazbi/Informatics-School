739a319d590f8e19c334cae99c49587d704daafc
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-28 23:29:17 +0430

    add new login Page and complete desktop design

---
+++ script/login.js
@@ -0,0 +1,33 @@
+$(function() {
+	'use strict';
+	
+  $('.form-control').on('input', function() {
+	  var $field = $(this).closest('.form-group');
+	  if (this.value) {
+	    $field.addClass('field--not-empty');
+	  } else {
+	    $field.removeClass('field--not-empty');
+	  }
+	});
+
+});
+
+document.getElementById("icon-element").addEventListener("click", toggleIcon);
+function toggleIcon() {
+	// show password by click
+	const passwordElement = document.getElementById("password");
+	if (passwordElement.type === "password") {
+				  passwordElement.type = "text";
+	} else {
+				  passwordElement.type = "password";
+	}
+	// change icon by click
+	const iconElement = document.getElementById("icon-element");
+	if (iconElement.classList.contains("bi-eye-fill")) {
+	  iconElement.classList.remove("bi-eye-fill");
+	  iconElement.classList.add("bi-eye-slash-fill");
+	} else {
+	  iconElement.classList.remove("bi-eye-slash-fill");
+	  iconElement.classList.add("bi-eye-fill");
+	}
+  }
