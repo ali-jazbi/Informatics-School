38c2459031b7b1ebf9ab2bb7c34d27afc061ef2f
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-30 00:34:48 +0430

    Update loader.js

--- script/loader.js
+++ script/loader.js
@@ -1,11 +1,4 @@
 document.querySelector('main').style.display = 'none';
-// Hide overflow initially
-document.body.style.overflow = 'hidden';
-document.addEventListener('DOMContentLoaded', function () {
-        setTimeout(function () {
-                document.body.style.overflow = 'visible';
-        }, 10)
-});
 window.addEventListener('load', function(){
         document.querySelector('.loader').classList.add('hide');
         document.querySelector('main').style.display = 'block'; 
