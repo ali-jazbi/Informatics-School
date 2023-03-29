69bde650b3217e3375ff3093e179de7bdc118e70
Ali Jazbi <ali.jazbi@yahoo.com>
2023-03-30 00:26:19 +0430

    test for fixing scroll

--- script/loader.js
+++ script/loader.js
@@ -1,12 +1,12 @@
 document.querySelector('main').style.display = 'none';
-window.addEventListener('load', function(){
-        document.querySelector('.loader').classList.add('hide');
-        document.querySelector('main').style.display = 'block'; 
-});
 // Hide overflow initially
 document.body.style.overflow = 'hidden';
 document.addEventListener('DOMContentLoaded', function () {
         setTimeout(function () {
                 document.body.style.overflow = 'visible';
         }, 10)
+});
+window.addEventListener('load', function(){
+        document.querySelector('.loader').classList.add('hide');
+        document.querySelector('main').style.display = 'block'; 
 });
