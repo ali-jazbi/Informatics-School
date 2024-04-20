336a11c27da4d838fe3786fe712c96b4883c0fc9
Ali Jazbi <ali.jazbi@yahoo.com>
2024-04-21 00:25:02 +0330

    add new banner

--- script/style-header.js
+++ script/style-header.js
@@ -1,39 +1,29 @@
 new FinisherHeader({
-    "count": 90,
-    "size": {
-      "min": 1,
-      "max": 32,
-      "pulse": 0
+  count: 90,
+  size: {
+    min: 1,
+    max: 32,
+    pulse: 0,
+  },
+  speed: {
+    x: {
+      min: 0,
+      max: 0.3,
     },
-    "speed": {
-      "x": {
-        "min": 0,
-        "max": 0.3
-      },
-      "y": {
-        "min": 0,
-        "max": 0.1
-      }
+    y: {
+      min: 0,
+      max: 0.1,
     },
-    "colors": {
-      "background": "#008545",
-      "particles": [
-        "#ffffff",
-        "#87ddfe",
-        "#acaaff",
-        "#1bffc2",
-        "#f88aff"
-      ]
-    },
-    "blending": "screen",
-    "opacity": {
-      "center": 0,
-      "edge": 0.25
-    },
-    "skew": 0,
-    "shapes": [
-      "c",
-      "s",
-      "t"
-    ]
-  });
+  },
+  colors: {
+    background: "#008545",
+    particles: ["#ffffff", "#87ddfe", "#acaaff", "#1bffc2", "#f88aff"],
+  },
+  blending: "screen",
+  opacity: {
+    center: 0,
+    edge: 0.25,
+  },
+  skew: 0,
+  shapes: ["c", "s", "t"],
+});
